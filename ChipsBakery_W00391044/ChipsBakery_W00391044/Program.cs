254303a0e448e4c385f5b132f86b651c5b567a3c
Shiva <shiva5130@gmail.com>
2025-02-25 13:52:38 -0500

    assignment 1 solution pusing to git.

---
+++ ChipsBakery_W00391044/ChipsBakery_W00391044/Program.cs
@@ -0,0 +1,18 @@
+﻿start();
+
+static void start()
+{
+    int muffinsCount;
+    int cookiesCount;
+    const double price = 2.75;
+
+    Console.WriteLine("Welcome to Chips Bakery \r\nWe have muffins and cookies");
+    Console.Write("How many muffins would you like? ");
+    muffinsCount= Convert.ToInt16(Console.ReadLine());
+    Console.Write("How many cookies would you like? ");
+    cookiesCount = Convert.ToInt16(Console.ReadLine());
+
+    Console.WriteLine($"Okay great.  You ordered {muffinsCount} muffins and {cookiesCount} cookies.");
+    Console.WriteLine($"That will cost ${(muffinsCount+cookiesCount)*price}");
+
+}
