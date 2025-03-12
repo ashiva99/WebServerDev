7dd7b42cc810eba47ad7cc0eab4fd210b92247f7
Shiva <shiva5130@gmail.com>
2025-03-12 08:49:57 -0400

    pushing random numbers assignment

---
+++ RandomNumbers_W00391044/RandomNumbers_W00391044/Program.cs
@@ -0,0 +1,28 @@
+﻿start();
+
+static void start()
+{
+    int num;
+    Console.Write("How many random numbers do you want to generate today? (Between 1 and 20): ");
+
+    num = Convert.ToInt16(Console.ReadLine());
+
+    if (num > 0 && num <= 20)
+    {
+        for (int i = 0; i<num; ++i)
+        {
+            Console.WriteLine(RandomNumberGenerator());
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please try again");
+        start();
+    }
+}
+
+static int RandomNumberGenerator()
+{
+    Random rnd = new Random();
+    return rnd.Next(1,100);
+}
