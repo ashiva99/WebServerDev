2d46c7036dcdee58c732245106b6554454875d1a
Shiva <shiva5130@gmail.com>
2025-03-17 00:50:20 -0400

    a challenge task

---
+++ CodeChallenge_W00391044/CodeChallenge_W00391044/Program.cs
@@ -0,0 +1,26 @@
+﻿Start();
+static void Start()
+{
+    int count = 0;
+    Console.WriteLine("Enter count");
+    int lines = int.Parse(Console.ReadLine());
+    while (count!=lines*2)
+    {
+        // Print
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write("-X");
+            Thread.Sleep(100);
+        }
+
+        // move to next line
+        Console.WriteLine();
+        count++;
+
+        // blank line after every 2 lines
+        if (count % 2 == 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
